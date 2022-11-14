fe1e1d718cecc308f825e07d7ff31532a88e7b86
pitteam11 <p3trogoncharov@yandex.ru>
2022-11-14 20:37:22 +0300

    Preprocessor

--- lesson1411/lesson1411.cpp
+++ lesson1411/lesson1411.cpp
@@ -1,20 +1,88 @@
-﻿// lesson1411.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "my_test.h"
+#include <time.h>
+#include <vector>
 
-#include <iostream>
+#define DEBUG
+
+#define QUBE(X) (X)*(X)*(X)
+#define RAND(min, max) (rand() % ((max) - (min) + 1) + (min))
+
+#define concat(a,b) a##b
+
+#define MYFACTORY(name, num) int name##num(){return num;}
+
+int fun1() {
+    int xy = 10;
+    // Для компилятора cout<<xy;
+    // на экране 10
+    std::cout << concat(x, y); //xy;
+    std::cout << xy;
+    return 0;
+}
+
+//#define int int64_t
+
+#define clocal
+
+const int global_const = 15;
+
+int my_rand(int min, int max) {
+    return rand() % ((max)-(min)+1) + (min);
+}
+
+MYFACTORY(new_fun, 1)
+MYFACTORY(new_fun, 2)
+MYFACTORY(new_fun, 3)
+MYFACTORY(new_fun, 4)
+
 
 int main()
 {
+    std::cout << new_fun1() << std::endl;
+
+    std::cout << my_global_value << std::endl;
     std::cout << "Hello World!\n";
+    std::cout << "Hello " << MY_NAME << "!\n";
+    std::cout << QUBE(2+1) << std::endl; // 2+1*2+1*2+1
+    srand(time(0));
+    for (int k = 0; k < 10; k++) {
+        std::cout << RAND(-10, 10) << " ";
+    }
+    std::cout << std::endl;
+    
+    int arr[5]{ 1,2,3,4,5 };
+    std::vector<int> my_vector(5);
+        
+    /*
+    my_vector[0] = 12;    
+    my_vector[1] = 13;
+    my_vector[2] = 7;
+    my_vector[3] = 4;
+    my_vector[4] = 1;
+    */
+
+    typedef std::vector<std::vector<int>> matr;
+    matr Matr;
+    
+    for (int k = 0; k < my_vector.size(); k++) {
+        std::cout << my_vector[k] << " ";
+    }
+    std::cout << std::endl;
+
+    if (0) {
+
+    }
+#if 0 
+#error Это ошибка
+
+    //#ifndef // если макрос DEBUG - НЕ обявлен, то выполняем до #endif или #else до 
+
+//#ifdef DEBUG  // если макрос DEBUG - обявлен, то выполняем до #endif или #else до 
+
+#else
+
+#endif // DEBUG
+
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ lesson1411/my_test.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MY_NAME "Petro"
+//#define CONSOLE std::cout
+//#define Begin {
+//#define End }
+
+
+//#ifndef MY_TESTH
+//#define MY_TESTH
+
+int my_global_value = 555;
+
+//#endif
